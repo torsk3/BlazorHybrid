c5e94960e7304e1b5c43703d3bab80b263e2a136
torsk3 <tamagorou888@gmail.com>
2024-11-16 23:44:35 +0900

    カンバン追加とクリックイベント追加

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.WebView.WindowsForms;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
 
 namespace BlazorHybrid
 {
@@ -12,6 +13,7 @@ namespace BlazorHybrid
             var services = new ServiceCollection();
             services.AddWindowsFormsBlazorWebView();
             services.AddHttpClient();
+            services.AddMudServices();
             blazorWebView1.HostPage = "wwwroot\\index.html";
             blazorWebView1.Services = services.BuildServiceProvider();
             blazorWebView1.RootComponents.Add<App>("#app");
